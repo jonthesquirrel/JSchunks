8e3503d078de4ebeb7b5d744f73b78acb0b1ff26
Jonathan Herman <jdh11235@gmail.com>
2014-11-02 20:30:38 -0600

    readability: Ftemp => F, function keeps F in scope

--- fibonacci.js
+++ fibonacci.js
@@ -4,11 +4,11 @@ function fib( n ) {
 
 function F( n ){
 
-	var Ftemp = [0, 1];
+	var F = [0, 1];
 	
 	for( i = 2; i <= n; i++ ){
-		Ftemp[i] = Ftemp[i - 2] + Ftemp[i - 1]
+		F[i] = F[i - 2] + F[i - 1]
 	}
 
-	return Ftemp;
+	return F;
 }
