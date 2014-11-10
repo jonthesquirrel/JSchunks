ab1436abc473beae09b1737d1e8c88a760dd175f
Jonathan Herman <jdh11235@gmail.com>
2014-11-09 21:29:57 -0600

    two methods

--- querySelector.js
+++ querySelector.js
@@ -1,5 +1,6 @@
 /* Copyright (c) 2014 Jonathan Herman - MIT License (http://opensource.org/licenses/MIT) */
 
+//returns array if multiple items, item if single item
 function $(s) {
 	var a = document.querySelectorAll(s);
 	if (a.length <= 1) {
@@ -7,4 +8,9 @@ function $(s) {
 	} else {
 		return a;
 	}
+}
+
+//always returns array
+function $(s) {
+	return document.querySelectorAll(s);
 }
