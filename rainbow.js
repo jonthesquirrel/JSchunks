8a0dc9e5d780bd175e2b6e3c5017d44515ccba98
Jonathan Herman <jdh11235@gmail.com>
2014-11-21 04:24:51 -0600

    neater functions

--- rainbow.js
+++ rainbow.js
@@ -1,9 +1,18 @@
 /* Copyright (c) 2014 Jonathan Herman - MIT License (http://opensource.org/licenses/MIT) */
 
-function randRange(min, max) {return Math.random() * (max - min) + min};
-function randInt(min, max) {return Math.round(randRange(min, max))};
-function randIntString(min, max) {return randInt(min, max).toString()};
-function randColor() {return 'rgb(' + randIntString(0, 256) + ', ' + randIntString(0, 256) + ', ' + randIntString(0, 256) + ')'};
+function randRange(min, max) {
+	return Math.random() * (max - min) + min
+}
+function randInt(min, max) {
+	return Math.round(randRange(min, max))
+}
+function randIntString(min, max) {
+	return randInt(min, max).toString()
+}
+function randColor() {
+	return 'rgb(' + randIntString(0, 256) + ', ' + randIntString(0, 256) + ', ' + randIntString(0, 256) + ')'
+}
+
 function rainbow(elementArray){
 	for each (var elm in elementArray) {
 		elm.style.backgroundColor = randColor();
