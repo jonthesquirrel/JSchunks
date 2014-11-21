104c514a214a85524134b54fa10802796d992c9b
Jonathan Herman <jdh11235@gmail.com>
2014-11-21 04:20:57 -0600

    awesome disco mode

--- rainbow.js
+++ rainbow.js
@@ -1,11 +1,18 @@
 /* Copyright (c) 2014 Jonathan Herman - MIT License (http://opensource.org/licenses/MIT) */
 
-(function(){
-	function randRange(min, max) {return Math.random() * (max - min) + min};
-	function randInt(min, max) {return Math.round(randRange(min, max))};
-	function randIntString(min, max) {return randInt(min, max).toString()};
-	function randColor() {return 'rgb(' + randIntString(0, 256) + ', ' + randIntString(0, 256) + ', ' + randIntString(0, 256) + ')'};
-	function rainbow(elm) {elm.style.backgroundColor = randColor()};
+function randRange(min, max) {return Math.random() * (max - min) + min};
+function randInt(min, max) {return Math.round(randRange(min, max))};
+function randIntString(min, max) {return randInt(min, max).toString()};
+function randColor() {return 'rgb(' + randIntString(0, 256) + ', ' + randIntString(0, 256) + ', ' + randIntString(0, 256) + ')'};
+function rainbow(elementArray){
+	for each (var elm in elementArray) {
+		elm.style.backgroundColor = randColor();
+		elm.style.color = randColor();
+	}
+}
 
-	setInterval(function() {rainbow(document.body)}, 200);
-})();
+//sane example
+rainbow([document.body]);
+
+//disco example
+setInterval(function() {rainbow(document.querySelectorAll('*'))}, 200);
