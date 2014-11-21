b16bc712f77fb1eb3eedc1cf56202861cd570c3b
Jonathan Herman <jdh11235@gmail.com>
2014-11-21 04:10:50 -0600

    very confused. CORS sucks.

--- MarkletBootstrap.js
+++ MarkletBootstrap.js
@@ -1,17 +1,20 @@
-function useMarklet(marklet) {
-	alert(marklet);
+function useMarklet(title, marklet) {
+	var elm = document.createElement('p');
+	elm.innerHTML = title;
+	elm.href = marklet;
+	document.body.appendChild(elm);
 }
 
-function MarkletBootstrap(callback, template_location, $URL, $NAMESPACE) {
+function MarkletBootstrap(callback, title, template_location, $URL, $NAMESPACE) {
 	var request = new XMLHttpRequest();
 	request.onload = function() {
 		var text = request.responseText;
 		text = text.replace(/\$URL/g, $URL);
 		text = text.replace(/\$NAMESPACE/g, $NAMESPACE);
-		callback(text);
+		callback(title, text);
 	};
 	request.open('get', template_location);
 	request.send();
 }
 
-MarkletBootstrap(useMarklet, 'MarkletBootstrap_template.js', 'http://username.github.io/marklets/example.js', 'username_markletname');
+MarkletBootstrap(useMarklet, 'GiveBox', '//hypersphere.localMarkletBootstrap_template.js', 'http://username.github.io/marklets/example.js', 'username_markletname');
