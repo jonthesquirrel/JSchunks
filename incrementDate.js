d65269fe1c193f700d8af54dde6fcfbbb78dd398
Jonathan Herman <jdh11235@gmail.com>
2015-03-22 15:55:14 -0500

    incrementDate.js

---
+++ incrementDate.js
@@ -0,0 +1,19 @@
+var incrementDate = function incrementDate(date_str, incrementor) {
+    var parts = date_str.split("-");
+    var dt = new Date(
+        parseInt(parts[0], 10),      // year
+        parseInt(parts[1], 10) - 1,  // month (starts with 0)
+        parseInt(parts[2], 10)       // date
+    );
+    dt.setTime(dt.getTime() + incrementor * 86400000);
+    parts[0] = "" + dt.getFullYear();
+    parts[1] = "" + (dt.getMonth() + 1);
+    if (parts[1].length < 2) {
+        parts[1] = "0" + parts[1];
+    }
+    parts[2] = "" + dt.getDate();
+    if (parts[2].length < 2) {
+        parts[2] = "0" + parts[2];
+    }
+    return parts.join("-");
+}
